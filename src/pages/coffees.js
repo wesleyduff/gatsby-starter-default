5700a5b667b317456bb336cff3b98708e8cdcb79
wesley.duff@charter.com <wesley.duff@charter.com>
2022-02-07 14:17:54 -0700

    (update) testing change

--- src/pages/coffees.js
+++ src/pages/coffees.js
@@ -20,7 +20,7 @@ export const query = graphql`
 const CoffeesPage = ({data}) => (
     <div>
         <h1>Different types of coffee</h1>
-        { data.allNodeCoffee && data.allNodeCoffee.edges && data.allNodeCoffee.edges.length > 1 && data.allNodeCoffee.edges.map(({ node }) => (
+        { data.allNodeCoffee.edges.map(({ node }) => (
             <div>
                 <h3>{ node.title }</h3>
                 <div dangerouslySetInnerHTML={{ __html: node.body.value }} />
